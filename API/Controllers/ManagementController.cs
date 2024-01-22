6e6c571742df3931fb6d27528f2f3535e9783da4
Adi Tenea <tenea07adi@yahoo.com>
2024-01-23 00:24:08 +0200

    - Added the management controller - Added UpdateDatabase endpoint

---
+++ API/Controllers/ManagementController.cs
@@ -0,0 +1,34 @@
+﻿using DatabaseLayer.DatabaseConnection;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagementController : ControllerBase
+    {
+        private readonly DataBaseContext _dataBaseContext;
+
+        public ManagementController(DataBaseContext dataBaseContext)
+        {
+            _dataBaseContext = dataBaseContext;
+        }
+
+        [HttpGet("UpdateDatabase")]
+        public IActionResult RunDatabaseUpdate()
+        {
+            try
+            {
+                _dataBaseContext.Database.Migrate();
+                return Ok("Success!");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    
+    }
+}
